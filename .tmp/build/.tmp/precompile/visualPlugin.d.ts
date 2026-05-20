bf87f86be0919fc49d80a47b7d1100f5f5ca5e84
Gnapthika Thanneeru <gnapthika.t@brillio.com>
2026-05-20 14:19:32 +0530

    Remove temp files

--- .tmp/build/.tmp/precompile/visualPlugin.d.ts
+++
@@ -1,4 +0,0 @@
-import powerbiVisualsApi from "powerbi-visuals-api";
-import IVisualPlugin = powerbiVisualsApi.visuals.plugins.IVisualPlugin;
-declare var agentPythonProxyChat5D2110A8D4D64FDEAF377B7C821B2E51: IVisualPlugin;
-export default agentPythonProxyChat5D2110A8D4D64FDEAF377B7C821B2E51;
